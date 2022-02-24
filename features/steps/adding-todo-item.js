11959acbfc4f99cd24c7c72eaebd3afe7afeed6f
unsalsenturkk <unsalsenturkk@gmail.com>
2022-02-24 16:49:18 +0300

    Updated given step for adding-todo-item feature

--- features/steps/adding-todo-item.js
+++ features/steps/adding-todo-item.js
@@ -1,7 +1,14 @@
 const {Given, When, Then} = require("cucumber");
+const openUrl = require("../support/action/openUrl")
+const waitForSelector = require("../support/action/waitForSelector")
+const checkContainsText = require("../support/check/checkContainsText")
 
-Given(/^Empty ToDo list$/, function () {
 
+Given(/^Empty ToDo list$/, async function () {
+    await openUrl.call(this, "/")
+    await waitForSelector.call(this, '#todolist')
+    const selector = "#todolist"
+    await checkContainsText.call(this, selector, false, "")
 });
 
 When(/^I write "([^"]*)" to (.*) and click to (.*)$/, function (arr,txt,btn) {
