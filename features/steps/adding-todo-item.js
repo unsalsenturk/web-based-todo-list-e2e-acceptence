809e3c682dcecbe456decdc781959ed72650b4a6
unsalsenturkk <unsalsenturkk@gmail.com>
2022-02-25 15:38:02 +0300

    Added when step for features/steps/adding-todo-item

--- features/steps/adding-todo-item.js
+++ features/steps/adding-todo-item.js
@@ -1,6 +1,7 @@
 const {Given, When, Then} = require("cucumber");
 const openUrl = require("../support/action/openUrl")
 const waitForSelector = require("../support/action/waitForSelector")
+const clickElement = require("../support/action/clickElement")
 const {strict: assert} = require("assert");
 
 Given(/^Empty ToDo list$/, async function () {
@@ -8,18 +9,23 @@ Given(/^Empty ToDo list$/, async function () {
     const selector = '#todolist'
     await waitForSelector.call(this, selector)
     const elementText = await this.page.$eval(selector, el => el.textContent);
-    assert.strictEqual(elementText, "", `Expected "${selector}" to "" but had "${elementText}" instead`);});
+    assert.strictEqual(elementText, "", `Expected "${selector}" to "" but had "${elementText}" instead`);
+});
 
-When(/^I write "([^"]*)" to (.*) and click to (.*)$/,async function (arr,txt,btn) {
+When(/^I write "([^"]*)" to (.*) and click to (.*)$/, async function (arr, txt, btn) {
 
-    txtSelector = "addTxt"
-    btnSelector = "addBtn"
+    txtSelector = "#addTxt"
+    btnSelector = "#addBtn"
     await waitForSelector.call(this, txtSelector)
     await waitForSelector.call(this, btnSelector)
 
-    for (let [listItem] of arr.rawTable) {
-        console.log(listItem)
-    }
+    const elementText = await this.page.$eval(txtSelector, async (el, arr) => {
+            el.value = arr
+            el.innerHTML = arr
+            return el.textContent
+        },
+        arr);
+    await clickElement.call(this,btnSelector)
 
 });
 Then(/^I should see "([^"]*)" item in ToDo list$/, function (arr) {
